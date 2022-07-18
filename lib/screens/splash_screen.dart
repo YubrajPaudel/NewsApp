d98b1cfc74d6b3e20ca3a9b225cf1c045808304e
Yubraj Paudel <yubrajp2@gmail.com>
2022-07-18 20:45:17 +0545

    Splash screen created

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import '../utils/constant.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size screenSize = getScreenSize();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: screenSize.height,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("/images/frame5.png"),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'We show news for you',
+                    style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 179, 131, 2)),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 300,
+                    width: 300,
+                    child: Image.asset('/images/news.png'),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RaisedButton(
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(16.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const HomePage(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Skip >>',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color yellowColor = Color(0xfffed813); //Yellow
+
+const Color activeCyanColor = Color(0xff0a7c97);
+
+const Color backgroundColor = Color(0xffebecee);
+
+Size getScreenSize() {
+  return MediaQueryData.fromWindow(WidgetsBinding.instance.window).size;
+}
